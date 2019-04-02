8bccdc3b02df111d6f9277fbb73410051f5a6642
smurphy8 <stephen.murphy1@northwestern.edu>
2019-04-02 12:45:58 -0500

    Code now reads in variable data and also recenters map depending on dropdpwn

--- static/js/app.js
+++ static/js/app.js
@@ -1,64 +1,70 @@
 
-
 var globalPlaceNames;
-function recenterMap(lat, long) {
 
-  console.log(lat, long);
-
-}
 function init() {
-  // Create the tile layer that will be the background of our map
-      var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-        maxZoom: 10,
-        id: "mapbox.light",
-        accessToken: API_KEY
-      });
-
-      //Create a map centered on US 
-        var map = L.map("map-id", {
-        center: [39.8283, -98.5795],
-        zoom: 4.4,
-        
-      });
-
-      lightmap.addTo(map);
-  
-  
-  
-  // Grab a reference to the dropdown select element
- 
-
-
-  // Use the list of sample names to populate the select options
   d3.json("/places").then((placeNames) => {
     var i;
     for (i =0; i < placeNames.length; i++) {
       place = placeNames[i];
-      console.log(placeNames[i]);
+      
       d3.select("#selDataset")  
         .append("option")
         .text(place.Geography)
         .property("value", place.Geography);
     };
     globalPlaceNames = placeNames;
+
+    const firstPlace = placeNames[213].Geography;
+    const firstLat = placeNames[213].Latitude;
+    const firstLong = placeNames[213].Longitude;
+    getMap(firstPlace, firstLat, firstLong);
     
-    const firstPlace = placeNames[0].Geography;
-    const firstLat = placeNames[0].Latitude;
-    const firstLong = placeNames[0].Longitude;
-    recenterMap(firstLat, firstLong);
-    // marker(firstPlace);
   });
 }
 
-function optionChanged(newPlace)
- {
+// function income() {
+//   d3.json("/income/<yearIndex>").then((incomeData) => {
+//     var i;
+//     for (i=0; i < incomeData.length; i++) {
+//     }  
+
+
+//   });
+// }
+
+
+function optionChanged(newPlace){
   thisPlace = globalPlaceNames.find(d => d.Geography == newPlace);
 
-  console.log(thisPlace);
-  
-  // recenterMap(newPlace.Latitude, newPlace.Longitude);
-  //  marker(newPlace);
+  deleteMap();
+  getMap(thisPlace.Geography, thisPlace.Latitude, thisPlace.Longitude);
 } 
+
+function deleteMap() {
+    d3.select("#map-id").remove();
+    d3.select("body").append("div").attr("id", "map-id");
+}
+
+function getMap(place, lat, long) {
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 10,
+    id: "mapbox.light",
+    accessToken: API_KEY
+  });
+
+  //Create a map centered on US 
+  
+  var map = L.map("map-id", {
+    center: [lat, long],
+    zoom: 6,   
+    });
+
+  
+lightmap.addTo(map);
+
+
+}
+
 
 init();
